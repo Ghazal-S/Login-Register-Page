4dba35667ff1ee4ff612c9b37b2412a385719a45
Ghazal-S <ghazalsadeghi1751@gmail.com>
2023-07-02 11:34:12 +0330

    commit-2

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,65 +1,77 @@
 import React, { useState } from "react";
-import styles from "./login.modiule.scss";
-import { BiRightArrowAlt } from "react-icons/bi";
+import styles from "./login.module.scss";
 import { Icon } from "@iconify/react";
 
-export const Login = (props) => {
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+export const Login = ({ onFormSwitch }) => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const onChange = (e) =>
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+  const handleSubmit = (e) => e.preventDefault();
 
   return (
-    <div className="auth-form">
-      <div className="heading">
-        <Icon icon="entypo:login" color="#7979ff" width="30" height="30" />
-        <h1>welcome!</h1>Sign in to your account</div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">E-mail </label>
-        <div className="d-flex">
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Icon
-            icon="ic:baseline-email"
-            color="#b3b3b3"
-            width="14"
-            height="14"
-          />
+    <div className={styles["auth-form"]}>
+      <div className={styles["heading"]}>
+        <Icon
+        icon="entypo:login"
+        color="#7979ff"
+        width="30"
+        height="30"
+      />
+      <h1>welcome!</h1>Sign in to your account
+    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">E-mail </label>
+      <div className="d-flex">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={inputs.email}
+          onChange={onChange}
+        />
+        <Icon
+          icon="ic:baseline-email"
+          color="#b3b3b3"
+          width="14"
+          height="14"
+        />
         </div>
         <label htmlFor="password">Password</label>
         <div className="d-flex">
           <input
             type="password"
             id="password"
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
+            name="password"
+            value={inputs.password}
+            onChange={onChange}
           />
           <Icon 
-            icon="mdi:eye"
-            color="#b3b3b3" 
-            width="14" 
-            height="14" />
+          icon="mdi:eye" 
+          color="#b3b3b3" 
+          width="14" 
+          height="14"
+          />
         </div>
-        <div className="login-pass">
-        <div>
+        <div className={styles["login-pass"]}>
+          <div>
             <input type="checkbox" />
             <label>remember me</label>
-           </div>
-            <a href="">forgot password?</a>
+          </div>
+          <a href="">forgot password?</a>
         </div>
         <button type="submit">
-          Log In <BiRightArrowAlt size={20} />
+          Log In <Icon icon="heroicons-solid:arrow-sm-right" width="20" height="20" />
         </button>
       </form>
       <button
-        className="switch-btn"
-        onClick={() => props.onFormSwitch("register")}>
+        className={styles["switch-btn"]}
+        onClick={() => onFormSwitch("register")}
+      >
         Don't have an account?
       </button>
     </div>
--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
+import styles from "./register.module.scss"
 import { Icon } from "@iconify/react";
-import { BiRightArrowAlt } from "react-icons/bi";
 
-export const Register = (props) => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+export const Register = ({ onFormSwitch }) => {
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const onChange = (e) =>
+  setInputs((prev) => ({...prev, [e.target.name]: e.target.value}));
+
+  const handleSubmit = e => e.preventDefault();
 
   return (
-    <div className="auth-form">
-      <div className="heading">
+    <div className={styles["auth-form"]}>
+      <div className={styles["heading"]}>
         <Icon
           icon="mingcute:user-4-fill"
           color="#7979ff"
@@ -28,18 +31,24 @@ export const Register = (props) => {
           <input
             type="text"
             id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            name="name"
+            value={inputs.name}
+            onChange={onChange}
           />
-          <Icon icon="fa-solid:user" color="#b3b3b3" width="10" height="10" />
-        </div>
-        <label htmlFor="email">E-mail</label>
-        <div className="d-flex">
+          <Icon icon="fa-solid:user"
+          color="#b3b3b3"
+          width="10"
+          height="10"
+         />
+         </div>
+         <label htmlFor="email">E-mail</label>
+         <div className="d-flex">
           <input
             type="email"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            value={inputs.email}
+            onChange={onChange}           
           />
           <Icon
             icon="ic:baseline-email"
@@ -53,8 +62,9 @@ export const Register = (props) => {
           <input
             type="password"
             id="password"
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
+            name="password"
+            value={inputs.password}
+            onChange={onChange}
           />
           <Icon
             icon="iconamoon:lock-fill"
@@ -64,12 +74,12 @@ export const Register = (props) => {
           />
         </div>
         <button type="submit">
-          Create <BiRightArrowAlt size={20} />
+          Create <Icon icon="heroicons-solid:arrow-sm-right" width="20" height="20" />
         </button>
       </form>
       <button
-        className="switch-btn"
-        onClick={() => props.onFormSwitch("login")}>
+        className={styles["switch-btn"]}
+        onClick={() => onFormSwitch("login")}>
         Already have an accuont?
       </button>
     </div>
